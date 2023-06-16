81a002a48293a2c9f01bab55efb263fd0b9667ad
Emmanuel Baah Duodu <duoduemmanuelbaah@gmail.com>
2023-06-16 15:43:16 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import noteReducer from './store/noteReducer';
 import './App.css';
+import Header from './components/Header';
+import NotesList from './components/NotesList';
 
-function App() {
+
+
+
+const store = createStore(noteReducer);
+
+const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const handleToggleDarkMode = () => {
+    setDarkMode((prevDarkMode) => !prevDarkMode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className={`App ${darkMode ? 'dark-mode' : 'light-mode'}`}>
+        <Header handleToggleDarkMode={handleToggleDarkMode} />
+        <NotesList />
+      </div>
+    </Provider>
   );
-}
+};
 
 export default App;
